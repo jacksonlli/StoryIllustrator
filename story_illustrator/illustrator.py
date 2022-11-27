0704c4e2a63d020ac4a461b98e560bec36e8fcc9
jacksonlli <59549394+jacksonlli@users.noreply.github.com>
2022-11-27 17:09:56 -0500

    V0: bug fixes

--- story_illustrator/illustrator.py
+++ story_illustrator/illustrator.py
@@ -1,4 +1,5 @@
 import os
+import math
 import time
 import json
 
@@ -24,9 +25,14 @@ class Illustrator:
             if selection_range and i not in selection_range:
                 continue
             start_time = time.time()
+            invalid_chars = ['<', '>', ':', '"', '/', '\\', '|', '?', '*', '\n']
+            subdir_name = token[:150]
+            for ch in invalid_chars:
+                subdir_name = subdir_name.replace(ch, '')
+
             out_subdir = os.path.join(
                 self.output_directory,
-                "-".join([str(i), token[:150].replace('"', "").replace(".", "")]),
+                "-".join([str(i).zfill(int(math.log10(len(tokens)))), subdir_name]),
             )
             prompt = (
                 token
